4c2f04d975a36e0cd177c8e156aea3e853fb910c
aigiarnc <aigiarn.cheuk@gmail.com>
2022-01-22 17:41:17 -0500

    big logo

---
+++ Android/app/src/main/java/com/WWCNT/stronkchonk/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.WWCNT.stronkchonk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        startActivity(new Intent(this, MainActivity.class));
+
+    }
+}
