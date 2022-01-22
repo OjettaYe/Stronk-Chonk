b51560ba7dc98277b31619f8b6de060a9cccab0a
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-01-22 14:00:07 -0500

    Implemented Activity Log List
    Implemented the Activity Log List.

--- Android/app/src/main/java/com/WWCNT/stronkchonk/Workout.java
+++ Android/app/src/main/java/com/WWCNT/stronkchonk/Workout.java
@@ -11,6 +11,15 @@ public class Workout {
     private int length;
     private int exp;
 
+    public Workout(int id, String name, Time startTime, Time endTime, int length, int exp) {
+        this.id = id;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.length = length;
+        this.exp = exp;
+    }
+
     //Get/Set Methods
     public void setId(int id){
         this.id = id;
