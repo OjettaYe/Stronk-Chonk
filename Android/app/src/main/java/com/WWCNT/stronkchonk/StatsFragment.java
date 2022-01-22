d383452b75092376db59db3f4520dd8ad7d2bfa1
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-01-22 17:22:54 -0500

    Updated the stats page
    Updated the stats page to retrieve the correct values for the squirrel.

--- Android/app/src/main/java/com/WWCNT/stronkchonk/StatsFragment.java
+++ Android/app/src/main/java/com/WWCNT/stronkchonk/StatsFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
 
 public class StatsFragment extends Fragment {
     private View statsView;
@@ -17,7 +21,16 @@ public class StatsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle("Stats");
         statsView = inflater.inflate(R.layout.fragment_stats, container, false);
+        //Test Squirrel
+        Squirrel s1 = new Squirrel(1,20,5,"MR. CHONKY BOI");
+        TextView tvName = (TextView) statsView.findViewById(R.id.squirrelName);
+        TextView tvSize = (TextView) statsView.findViewById(R.id.squirrelSize);
+        TextView tvChonkLevel = (TextView) statsView.findViewById(R.id.squirrelChonkLevel);
 
+
+        tvName.setText(s1.getName());
+        tvSize.setText(Integer.toString(s1.getSize()));
+        tvChonkLevel.setText(Integer.toString(s1.getChonklevel()));
         return statsView;
     }
 }
