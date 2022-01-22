d516754338315167d7a93e21bcca316572d2c88f
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-01-22 11:14:11 -0500

    Constructors for Squirrel, User, Workout
    Added the constructors for a squirrel, a user and a workout.

---
+++ Android/app/src/main/java/com/WWCNT/stronkchonk/User.java
@@ -0,0 +1,38 @@
+package com.WWCNT.stronkchonk;
+
+import java.util.ArrayList;
+
+public class User {
+    //Attributes.
+    private int id;
+    private String name;
+    private ArrayList<Workout> workouts;
+
+    //GetSet Methods;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    //NOTE: I don't think we should use these two get/set methods.
+    public void setWorkouts(ArrayList<Workout> workouts) {
+        this.workouts = workouts;
+    }
+    public ArrayList<Workout> getWorkouts() {
+        return workouts;
+    }
+    //Method to add a workout.
+    public void addWorkout(Workout workout){
+        workouts.add(workout);
+    }
+}
