a285f37e7cbe9104345a46c802e7fcdee3c13c22
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-01-23 02:13:08 -0500

    At Rest Squirrel Added

--- Android/app/src/main/java/com/WWCNT/stronkchonk/HomeFragment.java
+++ Android/app/src/main/java/com/WWCNT/stronkchonk/HomeFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomeFragment extends Fragment implements View.OnClickListener{
 
@@ -26,8 +27,13 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     private Button resetButton;
     private boolean running;
     private boolean atZero;
+    private TextView tvLong;
 
     private Chronometer chrono;
+    private long elapsedMillis = 0;
+
+    private Chronometer chronorest;
+    private int RestImage = 0;
 
     @Nullable
     @Override
@@ -50,7 +56,11 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
          chrono = homeView.findViewById(R.id.stopwatch);
          atZero = true;
 
-         return homeView;
+        tvLong = (TextView) homeView.findViewById(R.id.StoredLong);
+
+        chronorest = homeView.findViewById(R.id.stopwatchREST);
+        startStopwatchRest();
+        return homeView;
     }
 
     @Override
@@ -62,6 +72,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
                     startButton.setText("Start");
                 } else {
                     startStopwatch();
+                    chronorest.stop();
+                    chronorest.setBase(SystemClock.elapsedRealtime());
                     startButton.setText("Pause");
                 }
                 break;
@@ -71,8 +83,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     }
 
     private void startStopwatch() {
+        chrono.setVisibility(View.VISIBLE);
         if (atZero) {
             chrono.setBase(SystemClock.elapsedRealtime());
+        }
+        else{
+            chrono.setBase(SystemClock.elapsedRealtime() - elapsedMillis);
         }
         chrono.start();
 
@@ -86,18 +102,36 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
 
         atZero = false;
         running = true;
+        tvLong.setVisibility(View.INVISIBLE);
         resetButton.setVisibility(View.INVISIBLE);
     }
 
     private void pauseStopwatch() {
         running = false;
+        elapsedMillis = SystemClock.elapsedRealtime() - chrono.getBase();
+        chrono.setBase(SystemClock.elapsedRealtime());
         chrono.stop();
+        //Convert milli seconds to seconds/minutes
+        long minutes = (elapsedMillis/1000)/60;
+        long seconds = (elapsedMillis/1000)%60;
+        String minstring = String.format("%02d", minutes);
+        String secstring = String.format("%02d", seconds);
+        String output = minstring+":"+secstring;
+        chrono.setVisibility(View.INVISIBLE);
+
+//        TextView tvLong = (TextView) homeView.findViewById(R.id.StoredLong);
+        tvLong.setText(output);
+        tvLong.setVisibility(View.VISIBLE);
         resetButton.setVisibility(View.VISIBLE);
     }
 
     private void resetStopwatch() {
+        elapsedMillis = 0;
+        chrono.setVisibility(View.VISIBLE);
+        tvLong.setVisibility(View.INVISIBLE);
         chrono.setBase(SystemClock.elapsedRealtime());
         atZero = true;
+        startStopwatchRest();
     }
 
     private void toggleChonk(){
@@ -108,6 +142,47 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
             frame1.setImageResource(R.drawable.lifting_chonk_1);
             frame1vis = true;
         }
+    }
+    //Chonk at rest
+    private void toggleRestChonk(){
+        if (RestImage == 0){
+            frame1.setImageResource(R.drawable.squirrel0000);
+            RestImage++;
+        } else if (RestImage== 1){
+            frame1.setImageResource(R.drawable.squirrel0001);
+            RestImage++;
+        } else if (RestImage== 2){
+            frame1.setImageResource(R.drawable.squirrel0002);
+            RestImage++;
+        } else if (RestImage== 3){
+            frame1.setImageResource(R.drawable.squirrel0003);
+            RestImage++;
+        } else if (RestImage== 4){
+            frame1.setImageResource(R.drawable.squirrel0004);
+            RestImage++;
+        }else if (RestImage== 5){
+            frame1.setImageResource(R.drawable.squirrel0003);
+            RestImage++;
+        }else if (RestImage== 6){
+            frame1.setImageResource(R.drawable.squirrel0002);
+            RestImage++;
+        }else {
+            frame1.setImageResource(R.drawable.squirrel0001);
+            RestImage = 0;
+        }
+    }
+    private void startStopwatchRest() {
+        chronorest.setBase(SystemClock.elapsedRealtime());
+        chronorest.start();
+        chronorest.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                toggleRestChonk();
+            }
+        });
+//        chrono.setVisibility(View.INVISIBLE);
+        tvLong.setVisibility(View.INVISIBLE);
+        resetButton.setVisibility(View.INVISIBLE);
     }
 
 }
