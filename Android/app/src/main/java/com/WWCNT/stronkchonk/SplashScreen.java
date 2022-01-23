ef3c0d6d93f2073da8e0ccd7007ded8fd22643c3
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-01-22 19:28:46 -0500

    Fixed the splash screen

--- Android/app/src/main/java/com/WWCNT/stronkchonk/SplashScreen.java
+++ Android/app/src/main/java/com/WWCNT/stronkchonk/SplashScreen.java
@@ -3,6 +3,7 @@ package com.WWCNT.stronkchonk;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,8 +11,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
 
-        startActivity(new Intent(this, MainActivity.class));
+//        startActivity(new Intent(this, MainActivity.class));
 
     }
 }
